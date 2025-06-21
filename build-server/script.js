311d543658727d865f0afa0af7dc1e3c1a9a4661
Sheryar <alisharyar93@gmail.com>
2025-06-21 23:56:43 +0500

    ` Added S3 client and mime-types dependencies `

--- build-server/script.js
+++ build-server/script.js
@@ -1,7 +1,17 @@
 const { exec } = require('child_process')
 const Path = require('path')
 const fs = require('fs')
-const {} = require('@aws-sdk/client-s3')
+const mime = require('mime-types')
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3')
+
+const s3Client = new S3Client({
+    region: 'us-east-1',
+    credentials:{
+
+    }
+})
+
+const PROJECT_ID = process.env.PROJECT_ID || 'default-project-id'
 
 async function init() {
     console.log('Executing Script.js')
@@ -17,7 +27,7 @@ async function init() {
         console.log('Error', data.toString())
     })
 
-    p.stdout.on('close', function(data) {
+    p.stdout.on('close', async  function(data) {
         console.log('build complete! ')
         const distFolderPath = Path.join(__dirname,'output', 'dist')
         const distFolderContents = fs.readdirSync(distFolderPath, { recuresive: true })
@@ -25,7 +35,18 @@ async function init() {
     for (const file of distFolderContents) {
         if(fs.lstatSync(filePath).isDirectory()) continue; {
 
+            const command = new PutObjectCommand({
+                Bucket: '',
+                Key: `__output/${PROJECT_ID}/${filePath}`,
+                Body: fs.createReadStream(filePath),
+                ContentType: mime.lookup(filePath)
+
+            })
+
+            await s3Client.send(command)
         }
+
+        console.log(`Uploaded ${filePath} to S3`)
     }        
     })
 
